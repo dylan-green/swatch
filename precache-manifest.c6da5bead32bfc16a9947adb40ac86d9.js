adab02a56ddb5b6535d3bc1ef66899c5b695a4a6
Dylan Green <dylan.green61@gmail.com>
2020-02-01 14:51:46 -0700

    Updates

---
+++ precache-manifest.c6da5bead32bfc16a9947adb40ac86d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00e013e9e360a7c8157811d26146a05d",
+    "url": "/swatch/index.html"
+  },
+  {
+    "revision": "b3c74d44bcd902e5ad0c",
+    "url": "/swatch/static/css/main.ad9b375f.chunk.css"
+  },
+  {
+    "revision": "4c7601e1a9e2890c46f9",
+    "url": "/swatch/static/js/2.755f2149.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/swatch/static/js/2.755f2149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3c74d44bcd902e5ad0c",
+    "url": "/swatch/static/js/main.08834405.chunk.js"
+  },
+  {
+    "revision": "0ec51f7d90ac2a80f974",
+    "url": "/swatch/static/js/runtime-main.096faad1.js"
+  }
+]);
